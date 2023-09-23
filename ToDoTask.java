440e28f4a51c46a92114718f4afa11b092afb5d0
AgileMyk <90538513+AgileMyk@users.noreply.github.com>
2023-09-23 19:35:33 -0400

    initial task object form

---
+++ ToDoTask.java
@@ -0,0 +1,14 @@
+public abstract class ToDoTask {
+
+        private String name;
+        private String category;
+        private String priority;
+        private int totalTasks;
+        private double sessionTime;
+        private String color;
+
+        public ToDoTask(String name, String category, String priority, int totalTasks, double sessionTime, String color) {
+            this.name = name;
+        }
+
+}
