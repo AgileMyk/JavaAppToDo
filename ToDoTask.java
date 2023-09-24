4366bd6fa6e9e975c7352c5b0793128ce44cb4dd
AgileMyk <90538513+AgileMyk@users.noreply.github.com>
2023-09-23 20:06:33 -0400

    some subclass beginnings

--- ToDoTask.java
+++ ToDoTask.java
@@ -7,6 +7,10 @@ public abstract class ToDoTask {
         private double sessionTime;
         private String color;
 
+        public abstract void setTimer();
+        public abstract void startTimer();
+        public abstract void pausetimer();
+
         public ToDoTask(String name, String category, String priority, int totalTasks, double sessionTime, String color) {
             this.name = name;
         }
