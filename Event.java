3019ce885f61043bf0db4057c4fa2c2922ba9355
AgileMyk <90538513+AgileMyk@users.noreply.github.com>
2023-09-23 20:06:47 -0400

    some subclass beginnings

---
+++ Event.java
@@ -0,0 +1,11 @@
+public abstract class Event {
+
+    private String name;
+    private Date date;
+    private String type;
+    public Event(Date date, String name, String type) {
+        this.date = date;
+        this.name = name;
+        this.type = type;
+    }
+}
